7c1a5bbc498dd470147a6a6cebb43ca1f2c99256
pranavj1001 <pranavj1001@gmail.com>
2017-12-06 00:28:08 +0530

    intialize app with firebase config data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
+import firebase from 'firebase';
 import { createStore } from 'redux';
 import reducers from './reducers';
 
 const store = createStore(reducers);
 
 class App extends Component {
+
+  componentWillMount() {
+    // Initialize Firebase
+    const config = {
+      // your config details go here
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <Provider store={store}>
