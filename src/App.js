0b2570bb5bb99440d883425cee57c188da28f54b
pranavj1001 <pranavj1001@gmail.com>
2017-12-06 00:10:58 +0530

    add dummy reducer to start the app

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import reducers from './reducers';
 
-const store = createStore();
+const store = createStore(reducers);
 
 class App extends Component {
   render() {
