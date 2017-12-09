d36c330144f3d59bd10bae85c888c4ed8dd66464
pranavj1001 <pranavj1001@gmail.com>
2017-12-09 21:53:50 +0530

    add a new type in types.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,8 @@
 import firebase from 'firebase';
 import {
   EMAIL_CHANGED,
-  PASSWORD_CHANGED
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS
 } from './types';
 
 export const emailChanged = (text) => {
@@ -23,7 +24,7 @@ export const loginUser = (email, password) => {
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(user => {
         dispatch({
-          type: 'LOGIN_USER_SUCCESS',
+          type: LOGIN_USER_SUCCESS,
           payload: user
         });
       });
--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,17 +1,19 @@
 import {
   EMAIL_CHANGED,
-  PASSWORD_CHANGED
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS
 } from '../actions/types';
 
-const INITIAL_STATE = { email: '', password: '' };
+const INITIAL_STATE = { email: '', password: '', user: null };
 
 export default (state = INITIAL_STATE, action) => {
-  console.log(action);
   switch (action.type) {
     case EMAIL_CHANGED:
       return { ...state, email: action.payload };
     case PASSWORD_CHANGED:
       return { ...state, password: action.payload };
+    case LOGIN_USER_SUCCESS:
+      return { ...state, user: action.payload };
     default:
       return state;
   }
