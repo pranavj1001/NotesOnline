323bfa321b0a9fe6e19dd565baa07b614395ed80
pranavj1001 <pranavj1001@gmail.com>
2017-12-09 21:48:29 +0530

    test signIn of user

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,8 @@
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from './types';
+import firebase from 'firebase';
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED
+} from './types';
 
 export const emailChanged = (text) => {
   return {
@@ -11,5 +15,17 @@ export const passwordChanged = (text) => {
   return {
     type: PASSWORD_CHANGED,
     payload: text
+  };
+};
+
+export const loginUser = (email, password) => {
+  return (dispatch) => {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(user => {
+        dispatch({
+          type: 'LOGIN_USER_SUCCESS',
+          payload: user
+        });
+      });
   };
 };
--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -6,6 +6,7 @@ import {
 const INITIAL_STATE = { email: '', password: '' };
 
 export default (state = INITIAL_STATE, action) => {
+  console.log(action);
   switch (action.type) {
     case EMAIL_CHANGED:
       return { ...state, email: action.payload };
