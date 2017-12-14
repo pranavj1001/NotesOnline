a3ac68566c064b9ea6debba5ce3df59a71700a58
pranavj1001 <pranavj1001@gmail.com>
2017-12-14 22:32:37 +0530

    add NotesList Component

---
+++ src/components/NotesList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class NotesList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Testing List</Text>
+        <Text>Testing List</Text>
+        <Text>Testing List</Text>
+        <Text>Testing List</Text>
+        <Text>Testing List</Text>
+      </View>
+    );
+  }
+}
+
+export default NotesList;
--- src/router.js
+++ src/router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Stack, Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
+import NotesList from './components/NotesList';
 
 const RouterComponent = () => {
   return (
     <Router>
       <Stack key="root">
-        <Scene key="login" component={LoginForm} title="Please Login" />
+        <Scene key="login" component={LoginForm} title="Please Login" initial />
+        <Scene key="notesList" component={NotesList} title="Your Notes" />
       </Stack>
     </Router>
   );
