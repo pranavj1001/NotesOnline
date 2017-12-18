608624f958b96f8ee2c602a7021a86989e6e5c42
pranavj1001 <pranavj1001@gmail.com>
2017-12-18 11:49:32 +0530

    make a new file for AuthActions

---
+++ src/actions/AuthActions.js
@@ -0,0 +1,51 @@
+import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAILURE,
+  LOGIN_USER_START
+} from './types';
+
+export const emailChanged = (text) => {
+  return {
+    type: EMAIL_CHANGED,
+    payload: text
+  };
+};
+
+export const passwordChanged = (text) => {
+  return {
+    type: PASSWORD_CHANGED,
+    payload: text
+  };
+};
+
+export const loginUser = (email, password) => {
+  return (dispatch) => {
+    dispatch({ type: LOGIN_USER_START });
+    // login the user
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(user => loginUserSuccess(dispatch, user))
+      .catch(() => {
+        // if user is not present on the database then signup the user
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .then(user => loginUserSuccess(dispatch, user))
+          .catch(() => loginUserFailure(dispatch));
+      });
+  };
+};
+
+const loginUserSuccess = (dispatch, user) => {
+  dispatch({
+    type: LOGIN_USER_SUCCESS,
+    payload: user
+  });
+
+  Actions.main();
+};
+
+const loginUserFailure = (dispatch) => {
+  dispatch({ type: LOGIN_USER_FAILURE });
+};
--- src/reducers/NoteFormReducer.js
+++ src/reducers/NoteFormReducer.js
@@ -2,10 +2,17 @@ import {
   NOTE_UPDATE
 } from '../actions/types';
 
-const INITIAL_STATE = {};
+const INITIAL_STATE = {
+  title: '',
+  note: '',
+  phone: ''
+};
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case NOTE_UPDATE:
+      // action.payload === { prop: 'title' value: 'Shopping List' }
+      return { ...state, [action.payload.prop]: action.payload.value };
     default:
     return state;
   }
