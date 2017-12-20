ea54796ef693736efd272ad322e4d47210ff489c
pranavj1001 <pranavj1001@gmail.com>
2017-12-20 23:11:19 +0530

    wire up the reducer to the combineReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import AuthReducer from './AuthReducer';
 import NoteFormReducer from './NoteFormReducer';
+import NotesReducer from './NotesReducer';
 
 export default combineReducers({
   authentication: AuthReducer,
-  noteForm: NoteFormReducer
+  noteForm: NoteFormReducer,
+  notes: NotesReducer
 });
