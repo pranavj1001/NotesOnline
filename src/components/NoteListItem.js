5af3fcef32739da24b919905d8311d025ecc58b6
pranavj1001 <pranavj1001@gmail.com>
2017-12-21 23:37:02 +0530

    make the list item touchable

--- src/components/NoteListItem.js
+++ src/components/NoteListItem.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { Actions } from 'react-native-router-flux';
 import { CardSection } from './common';
 
 class NoteListItem extends Component {
+  onRowPress() {
+    Actions.createNewNote();
+  }
+
   render() {
     const { title } = this.props.notes;
 
     return (
-      <CardSection>
-        <Text style={styles.titleStyle}>
-          {title}
-        </Text>
-      </CardSection>
+      <TouchableWithoutFeedback onPress={this.onRowPress.bind(this)}>
+        <View>
+          <CardSection>
+            <Text style={styles.titleStyle}>
+              {title}
+            </Text>
+          </CardSection>
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
